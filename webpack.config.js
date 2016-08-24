514e9225df5a8d7f542f18bcfbbba04502c657ed
calebanth <caleb.m.anthony@gmail.com>
2016-08-24 12:23:23 -0600

    Minified output for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 module.exports = {
   entry: {
     Main: ['./js/main.js'],
@@ -8,7 +10,23 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
     ],
   },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
 }
